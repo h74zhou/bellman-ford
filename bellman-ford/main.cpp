f5ce42033bb1dab761ebf65f8596f9f2e1548490
Herun Zhou <iamherunzhou@gmail.com>
2019-04-04 15:44:28 -0400

    created new repo to implement bellman-ford algorithm

---
+++ bellman-ford/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bellman-ford
+//
+//  Created by Herun Zhou on 2019-04-04.
+//  Copyright © 2019 Herun Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
